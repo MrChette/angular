0b58893ef4f647b42b20ca18482728aba2e1eee0
Chette <80092129+MrChette@users.noreply.github.com>
2023-08-01 21:39:01 +0200

    Seccion 13 Finished

---
+++ 06-heroesApp/src/app/material/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+
+
+
+   ]
+})
+export class MaterialModule { }
