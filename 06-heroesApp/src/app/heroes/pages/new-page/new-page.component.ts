d5cd6fa08c07d8c6fe59070ea7a6ee45a2c82e54
Chette <80092129+MrChette@users.noreply.github.com>
2023-08-07 00:42:25 +0200

    CRUD HEROES 201-207
    EndPoints CRUD, validacion de formulario, enlace de form con la validacion

--- 06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Publisher } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-new-page',
@@ -8,9 +10,27 @@ import { Component } from '@angular/core';
 })
 export class NewPageComponent {
 
+  public heroForm = new FormGroup({
+    id:         new FormControl(''),
+    superhero:  new FormControl('', { nonNullable: true }),
+    publisher:  new FormControl<Publisher>( Publisher.DCComics ),
+    alter_ego:  new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img:    new FormControl(''),
+  });
+
+
   public publishers = [
     {id: 'DC Comics', desc: 'DC-Comics'},
     {id: 'Marvel Comics', desc: 'Marvel-Comics'},
   ]
+
+  onSubmit():void {
+    console.log({
+      formIsValid : this.heroForm.valid,
+      value: this.heroForm.value
+    })
+  }
 
 }
--- 06-heroesApp/src/app/heroes/services/heroes.service.ts
+++ 06-heroesApp/src/app/heroes/services/heroes.service.ts
@@ -2,7 +2,7 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from 'src/enviroments/environments.prod';
 
@@ -28,6 +28,22 @@ export class HeroesService {
 
   getSuggestions( query : string):Observable<Hero[]>{
     return this.http.get<Hero[]>(`${ this.baseUrl }/heroes?q=${query}&limit=6`);
+  }
+
+
+  addHero(hero : Hero):Observable<Hero> {
+    return this.http.post<Hero>(`${ this.baseUrl }/heroes`,hero);
+  }
+  updateHero(hero : Hero):Observable<Hero> {
+    if( !hero.id ) throw Error('Hero id is required')
+    return this.http.patch<Hero>(`${ this.baseUrl }/heroes/${hero.id}`,hero);
+  }
+  delHeroById(id : string):Observable<boolean> {
+    return this.http.delete(`${ this.baseUrl }/heroes`)
+      .pipe(
+        catchError( error => of(false)),
+        map( resp => true )
+      );
   }
 
 }
