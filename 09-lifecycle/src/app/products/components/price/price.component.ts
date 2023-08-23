acaed9ff1c0f385c489d7c87e86fbb76eaf29646
Chette <80092129+MrChette@users.noreply.github.com>
2023-08-23 18:32:21 +0200

    LifeCycle Hooks 271-274

---
+++ 09-lifecycle/src/app/products/components/price/price.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'products-price',
+  templateUrl: './price.component.html',
+  styleUrls: ['./price.component.css']
+})
+export class PriceComponent {
+
+}
