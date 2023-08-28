4f234ee6a070b46f4c7161fb8c0106d5d752557b
Chette <80092129+MrChette@users.noreply.github.com>
2023-08-28 12:46:01 +0200

    Directivas Personalizadas 309-313

---
+++ 11-directiveSignals/src/app/products/products.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductsRoutingModule } from './products-routing.module';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    ProductPageComponent
+  ],
+  imports: [
+    CommonModule,
+    ProductsRoutingModule,
+    ReactiveFormsModule,
+  ]
+})
+export class ProductsModule { }
